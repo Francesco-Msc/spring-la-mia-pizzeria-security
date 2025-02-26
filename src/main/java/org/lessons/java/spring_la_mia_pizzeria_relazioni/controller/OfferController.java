98953e345f27c01d4f116892a4f3453f4b30790d
Francesco-Msc <moscafrancesco1998@gmail.com>
2025-02-26 16:29:41 +0100

    added OfferController

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/controller/OfferController.java
@@ -0,0 +1,10 @@
+package org.lessons.java.spring_la_mia_pizzeria_relazioni.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/offers")
+public class OfferController {
+     
+}
