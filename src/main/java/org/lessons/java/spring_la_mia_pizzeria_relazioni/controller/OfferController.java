34c0d98be7809ac2ec3c2df05021d98f27be17e7
Francesco-Msc <moscafrancesco1998@gmail.com>
2025-02-26 18:25:39 +0100

    added store method in OfferController

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/controller/OfferController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/controller/OfferController.java
@@ -1,10 +1,34 @@
 package org.lessons.java.spring_la_mia_pizzeria_relazioni.controller;
 
+import org.lessons.java.spring_la_mia_pizzeria_relazioni.model.Offer;
+import org.lessons.java.spring_la_mia_pizzeria_relazioni.service.OfferService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/offers")
 public class OfferController {
-     
+    
+    @Autowired
+    private OfferService offerService;
+
+    @PostMapping("/create-offer")
+    public String store(@Valid @ModelAttribute("offer") Offer addOffer, BindingResult bindingResult, Model model){
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("create", true);
+            return "offers/create-edit-offer";
+        }
+
+        offerService.create(addOffer);
+        return "redirect:/homepage";
+    }
+
 }
