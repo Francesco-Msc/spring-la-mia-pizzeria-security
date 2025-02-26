e6992ece39828566a594ccb4ba15f28053357f0c
Francesco-Msc <moscafrancesco1998@gmail.com>
2025-02-26 18:26:06 +0100

    added create method in OfferService

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/service/OfferService.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/service/OfferService.java
@@ -1,5 +1,6 @@
 package org.lessons.java.spring_la_mia_pizzeria_relazioni.service;
 
+import org.lessons.java.spring_la_mia_pizzeria_relazioni.model.Offer;
 import org.lessons.java.spring_la_mia_pizzeria_relazioni.repository.OfferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ public class OfferService {
 
     @Autowired
     private OfferRepository offerRepo;
+
+    public Offer create(Offer addOffer){
+        return offerRepo.save(addOffer);
+    }
 }
