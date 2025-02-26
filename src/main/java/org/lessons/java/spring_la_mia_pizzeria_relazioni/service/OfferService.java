a45d56cb0ff65986c9873f6dc57f683b9aef23f2
Francesco-Msc <moscafrancesco1998@gmail.com>
2025-02-26 17:16:18 +0100

    added OfferService and autowired to OfferRepository

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/service/OfferService.java
@@ -0,0 +1,12 @@
+package org.lessons.java.spring_la_mia_pizzeria_relazioni.service;
+
+import org.lessons.java.spring_la_mia_pizzeria_relazioni.repository.OfferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OfferService {
+
+    @Autowired
+    private OfferRepository offerRepo;
+}
