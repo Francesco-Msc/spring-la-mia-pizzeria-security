dbd8fb322e205bc43f276a92311dc6c8209b837d
Francesco-Msc <moscafrancesco1998@gmail.com>
2025-03-08 12:03:28 +0100

    added SecurityConfig with filterChain, passwordEncoder, and authenticationProvider

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package org.lessons.java.spring_la_mia_pizzeria_relazioni.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    
+    @Bean
+    @SuppressWarnings("removal")
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/pizzas/create-edit", "/pizzas/create-edit/**").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.POST,  "/pizzas/**").hasAuthority("ADMIN")
+                .requestMatchers("/ingredients", "/ingredients/**").hasAuthority("ADMIN")
+                .requestMatchers("/pizzas/offer/**").hasAuthority("ADMIN")
+                .requestMatchers("/pizzas", "/pizzas/**").hasAnyAuthority("USER", "ADMIN")
+                .requestMatchers("/**").permitAll()
+                .and().formLogin()
+                .and().logout()
+                .and().exceptionHandling();
+        return http.build();
+    }
+
+    @Bean
+    DatabaseUserDetailsService userDetailsService(){
+        return new DatabaseUserDetailsService();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    
+}
