677980df3a264f9764b409ac76860ad24de6df7e
Francesco-Msc <moscafrancesco1998@gmail.com>
2025-03-09 21:46:28 +0100

    linked USER role to every new user

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/service/UserService.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/service/UserService.java
@@ -1,6 +1,8 @@
 package org.lessons.java.spring_la_mia_pizzeria_relazioni.service;
 
+import org.lessons.java.spring_la_mia_pizzeria_relazioni.model.Role;
 import org.lessons.java.spring_la_mia_pizzeria_relazioni.model.User;
+import org.lessons.java.spring_la_mia_pizzeria_relazioni.repository.RoleRepository;
 import org.lessons.java.spring_la_mia_pizzeria_relazioni.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,11 +17,22 @@ public class UserService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private RoleRepository roleRepo;
+
     public User createUser(String username, String rawPassword){
         String encodedPassword = passwordEncoder.encode(rawPassword);
         User newUser = new User();
         newUser.setUsername(username);
         newUser.setPassword(encodedPassword);
+
+        if (newUser.getRoles().size() == 0) {
+            Role userRole = roleRepo.findByRole("USER");
+            if (userRole != null) {
+                newUser.getRoles().add(userRole);
+            }
+        }
+
         return userRepo.save(newUser);
     }
 }
