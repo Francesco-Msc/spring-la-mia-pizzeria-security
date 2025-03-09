5c84f782f54c9e666de8a7bb7ec5b93b04921560
Francesco-Msc <moscafrancesco1998@gmail.com>
2025-03-09 21:45:14 +0100

    refactor: initialize roles set as a new HashSet

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/model/User.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/model/User.java
@@ -1,5 +1,6 @@
 package org.lessons.java.spring_la_mia_pizzeria_relazioni.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
@@ -33,7 +34,7 @@ public class User {
         joinColumns = @JoinColumn(name = "user_id"),
         inverseJoinColumns = @JoinColumn(name = "role_id")
     )
-    private Set<Role> roles;
+    private Set<Role> roles = new HashSet<>();
 
     public Integer getId() {
         return this.id;
